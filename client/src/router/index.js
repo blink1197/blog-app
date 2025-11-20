30fd43d3d24db5ead0ce15a3a0e5c3c55012eccf
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 17:47:47 +0800

    add not found page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -33,7 +33,17 @@ const router = createRouter({
       path: '/posts/:id',
       component: () => import('../views/ViewPost.vue')
     },
+    // Catch-all 404 route
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue'),
+    },
   ],
+  scrollBehavior() {
+    // Always scroll to top
+    return { top: 0 }
+  },
 })
 
 export default router
