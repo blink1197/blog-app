ff8870feb46b4d74bc40e048fad158ff13796fe9
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 09:32:03 +0800

    Create server entry point

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { errorHandler } = require("./middleware/errorHandler.js");
+require('dotenv').config()
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(    // Allow access from anywhere for now
+    cors({
+        origin: "*",
+    })
+);
+
+//MongoDB database connection
+mongoose.connect(process.env.MONGODB_STRING);
+mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
+
+//Routes
+const userRoutes = require("./routes/user.routes.js");
+const postRoutes = require("./routes/post.routes.js")
+
+
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+
+
+
+app.use(errorHandler);
+
+
+if (require.main === module) {
+    app.listen(process.env.PORT || port, () => {
+        console.log(`API is now online on port ${process.env.PORT || 4000}`)
+    });
+}
+
+module.exports = { app, mongoose };
