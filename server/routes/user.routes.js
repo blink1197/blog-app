de718377500666bb647506fda456094da85a5000
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 09:31:47 +0800

    Add user controller functions to routes

---
+++ server/routes/user.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { registerUser, loginUser, getProfile } = require("../controllers/user.controller.js");
+const { verify } = require("../middleware/auth.js");
+
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/details", verify, getProfile);
+
+
+module.exports = router;
