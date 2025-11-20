7721c4213a498e3f815d694ec791e9f7df6e21d1
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 09:31:27 +0800

    Create post controller functions

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,104 @@
+const Post = require("../models/post.model.js");
+const AppError = require("../utils/AppError.js");
+
+
+module.exports.addPost = async (req, res, next) => {
+    try {
+        const { title, content } = req.body;
+        const userId = req.user.id;
+
+        if (!title || !content) throw new AppError("Missing required fields", 400);
+
+        const post = await Post.create({
+            title,
+            content,
+            userId,
+        });
+
+        res.status(201).json(post);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.getPosts = async (req, res, next) => {
+    try {
+
+        const posts = await Post.find({});
+
+        res.status(200).json({ posts });
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+module.exports.getPostById = async (req, res, next) => {
+    try {
+        const { postId } = req.params;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            throw new AppError("Post not found", 404);
+        }
+
+        res.status(200).json(post);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports.updatePost = async (req, res, next) => {
+    try {
+        const { title, content } = req.body;
+        const { postId } = req.params;
+        const userId = req.user.id;
+
+        // Find post
+        const post = await Post.findOne({ _id: postId, userId });
+        if (!post) throw new AppError("Post not found or unauthorized", 404);
+
+
+        // Update only provided fields
+        if (title) post.title = title;
+        if (content) post.content = content;
+
+        await post.save();
+
+        res.status(200).json({
+            message: "Post updated successfully",
+            updatedPost: post._doc
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.deletePost = async (req, res, next) => {
+    try {
+        const { postId } = req.params;
+        const userId = req.user.id;
+
+        // Find and delete only if the post belongs to the user
+        const deletedPost = await Post.findOneAndDelete({
+            _id: postId,
+            userId
+        });
+
+        if (!deletedPost) {
+            throw new AppError("Post not found or unauthorized", 404);
+        }
+
+        res.status(200).json({
+            message: "Post deleted successfully",
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
