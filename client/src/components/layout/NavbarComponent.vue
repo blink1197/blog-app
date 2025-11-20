b3de42ccbea4aa9c839cbce15c4b5d9c7f81b359
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 17:47:56 +0800

    fix navbar link

--- client/src/components/layout/NavbarComponent.vue
+++ client/src/components/layout/NavbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
     <div class="container">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/">
         <img src="/images/logo.png" class="logo" />
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04"
