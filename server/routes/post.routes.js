3d11eafd0320fa6d6820e3b8e9dfc3ed6bdbcb4c
bagotsaycg <bagotsaycg@gmail.com>
2025-11-20 10:00:50 +0800

    Rearrange routes order

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const { addPost, getPosts, getPostById, updatePost, deletePost } = require("../controllers/post.controller.js")
-const { verify } = require("../middleware/auth.js");
+const {
+    addPost,
+    getPosts,
+    getPostById,
+    updatePost,
+    deletePost,
+    addComment,
+    deleteComment
+} = require("../controllers/post.controller.js");
 
+const { verify, verifyAdmin } = require("../middleware/auth.js");
 
 const router = express.Router();
 
-router.post("/add-post", verify, addPost);
+// Posts
 router.get("/", getPosts);
+router.post("/", verify, addPost);
+
+// Comments
+router.post("/:postId/comments", verify, addComment);
+router.delete("/:postId/comments/:commentId", verify, verifyAdmin, deleteComment);
+
+// Single post
 router.get("/:postId", getPostById);
 router.patch("/:postId", verify, updatePost);
-router.delete("/:postId", verify, deletePost);
-
+router.delete("/:postId", verify, verifyAdmin, deletePost);
 
 module.exports = router;
